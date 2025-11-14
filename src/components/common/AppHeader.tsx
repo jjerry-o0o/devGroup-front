56a014d47cfdaae12043a699c4c68985654e1af3
jjerry-o0o <alfndp25@gmail.com>
2025-11-14 13:30:44 +0900

    feat: Header 그림자 추가 및 login 버튼에 hover 스타일 추가

--- src/components/common/AppHeader.tsx
+++ src/components/common/AppHeader.tsx
@@ -3,26 +3,25 @@ import {Search} from 'lucide-react'
 
 function AppHeader() {
   return (
-    <header className="fixed top-0 z-10 w-full flex items-center justify-center bg-[#121212]">
+    <header className="fixed top-0 z-10 w-full flex items-center justify-center bg-white shadow-md">
       <div className="w-full max-w-[1600px] flex items-center justify-between px-4 py-3">
         {/* 좌측 */}
         <div className="flex gap-5 items-center">
-          <div className="font-semibold text-white text-2xl">DevGroup</div>
-          <div className="font-light text-white">Explore</div>
+          <div className="font-semibold text-black text-2xl">DevGroup</div>
+          <div className="font-medium text-[#24292e]">Explore</div>
           {/*<Separator orientation="vertical" className="!h-4"/>*/}
-          <div className="font-light text-white">Support</div>
+          <div className="font-medium text-[#24292e]">Support</div>
         </div>
         {/* 우측 */}
-        <div className="flex items-center gap-5">
+        <div className="flex items-center gap-3">
           {/*검색창*/}
-
-          <label htmlFor="search" className="flex border w-84 h-6 border-[#626262] rounded-sm justify-end items-center pr-2 pl-2">
+          <label htmlFor="search" className="flex border w-84 h-6 border-[#24292e] rounded-sm justify-end items-center pr-2 pl-2">
             <input id="serach" type="text" placeholder="Search repositories, images..."
-                   className="placeholder-[#626262] flex-1"/>
-            <Search color="#626262" size="16"/>
+                   className="placeholder-[#24292e] flex-1"/>
+            <Search color="#24292e" size="16"/>
           </label>
           {/*로그인 + 토글*/}
-          <div className="font-light text-muted-foreground hover:text-white transition-all duration-500">
+          <div className="text-[#24292e] hover:bg-[#667eea] hover:text-white px-2 py-1 rounded-sm transition-all duration-500">
             Login / Join
             <div className=""></div>
           </div>
