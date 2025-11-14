fd35db4264934fa2325953237726a00a43988909
jjerry-o0o <alfndp25@gmail.com>
2025-11-14 14:56:57 +0900

    feat: Main 페이지 작업중

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { AppHeader, AppFooter } from "@/components/common";
+import {Main} from "@/pages/Main.tsx"
 
 function App() {
 
-  return (
-    <div className="page">
-      <AppHeader />
-      <div className="container"></div>
-      <AppFooter />
-    </div>
-  );
+    return (
+        <div className="page">
+            <AppHeader/>
+            <Main/>
+            <div className="container"></div>
+            <AppFooter/>
+        </div>
+    );
 }
 
 export default App
---
+++ src/pages/Main.tsx
@@ -0,0 +1,14 @@
+function Main() {
+    return (
+        <section className="mt-[56px]  w-full flex flex-col">
+            <div className="h-50 mt-4 bg-linear-135 from-[#667eea] to-[#764ba2]">
+                <h1 className="text-white font-">
+                    Welcome To DevGroup!
+                </h1>
+            </div>
+        </section>
+
+    );
+}
+
+export {Main};
